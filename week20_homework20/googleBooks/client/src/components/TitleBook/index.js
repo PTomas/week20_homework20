441bc9180e1ff45665b75a7ae1dfb02104491bf7
Patrick Tomas <p.tomas14@gmail.com>
2021-02-01 16:57:08 -0800

    input message

---
+++ week20_homework20/googleBooks/client/src/components/TitleBook/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import API from "../utils/API";
+
+function TitleBook(props) {
+    const [book, setBook] = useState({});
+
+    const {id} = useParams()
+  useEffect(() => {
+    API.getBook(id)
+      .then(res => setBook(res.data))
+      .catch(err => console.log(err));
+  }, [])
+
+  return (
+    <div className="text-center">
+      <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
+      <h3>Authors(s): {props.author}</h3>
+      <h3>Description: {props.description}</h3>
+      <h3>Link: {props.link}</h3>
+    </div>
+  );
+}
+
+export default TitleBook;
